ef91960bdad3ee26437f30f6eb03ce785a20f244
Daniel Berger <dberger@redhat.com>
2017-08-17 07:42:32 -0500

    Updated copy_file method.

--- lib/azure/armrest/model/storage_account.rb
+++ lib/azure/armrest/model/storage_account.rb
@@ -326,18 +326,10 @@ module Azure
 
         headers = build_headers(dst_url, key, :file, options)
 
-        response = ArmrestService.send(
-          :rest_put,
-          :url         => dst_url,
-          :payload     => '',
-          :headers     => headers,
-          :proxy       => proxy,
-          :ssl_version => ssl_version,
-          :ssl_verify  => ssl_verify
-        )
+        response = rest_execute(url: dst_url, http_method: :put, headers: headers, use_token: false)
 
         Azure::Armrest::ResponseHeaders.new(response.headers).tap do |rh|
-          rh.response_code = response.code
+          rh.response_code = response.status
         end
       end
 
