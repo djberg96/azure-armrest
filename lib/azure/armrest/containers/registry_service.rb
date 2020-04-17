ee8ce5da06d3851d7ea4cb41d44591fd36ad0cc6
Daniel Berger <djberg96@gmail.com>
2020-04-17 14:30:22 -0400

    Update and rename available? method to check_availability.

--- lib/azure/armrest/containers/registry_service.rb
+++ lib/azure/armrest/containers/registry_service.rb
@@ -10,7 +10,27 @@ module Azure
           super(armrest_configuration, 'registries', 'Microsoft.ContainerRegistry', options)
         end
 
-        def available?(registry_name, resource_group)
+        # Check to see if the +registry_name+ is available for the subscription and return
+        # a Registry object. If true, the Registry object will contain a single property
+        # called +name_available+. If false, the resulting object will also contain a +reason+
+        # and a +message+ property.
+        #
+        def check_availability(registry_name)
+          url = url_with_api_version(
+            api_version,
+            base_url,
+            'providers',
+            provider,
+            'checkNameAvailability'
+          )
+
+          options = {
+            :name => registry_name,
+            :type => 'Microsoft.ContainerRegistry/registries'
+          }
+
+          response = rest_post(url, options.to_json)
+          Azure::Armrest::Containers::Registry.new(response)
         end
 
         def import_image(registry_name, resource_group, options)
